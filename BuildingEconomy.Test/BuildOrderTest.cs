da81e87836e3e1bd14d5d2224768a72d529013f9
Karsten Bock <KarstenBock@gmx.net>
2018-09-18 08:18:25 +0200

    - Added Build order and tests.

---
+++ BuildingEconomy.Test/BuildOrderTest.cs
@@ -0,0 +1,53 @@
+﻿using Akka.Actor;
+using Akka.TestKit.Xunit2;
+using BuildingEconomy.Systems;
+using BuildingEconomy.Systems.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xenko.Engine;
+using Xunit;
+
+namespace BuildingEconomy.Test
+{
+    public class BuildOrderTest : TestKit
+    {
+        class EntityTestActor : ReceiveActor
+        {
+            public EntityTestActor()
+            {
+                ReceiveAny(msg => Sender.Tell(msg));
+            }
+        }
+
+        [Fact]
+        public void TestValid()
+        {
+            // TODO: Add test after a builder component was added.
+        }
+
+        [Fact]
+        public void TestComplete()
+        {
+            var mockComponentActorFactory = new Mock<ComponentActorFactory<Components.ConstructionSite>>(Sys);
+            var constructionSite = new Components.ConstructionSite();
+            var buildOrder = new Systems.Orders.Build(constructionSite, mockComponentActorFactory.Object);
+            Assert.False(buildOrder.IsComplete(null));
+            constructionSite.CurrentStageProgress = 1.0f;
+            Assert.True(buildOrder.IsComplete(null));
+        }
+
+        [Fact]
+        void TestUpdate()
+        {
+            var entity = new Entity();
+            var mockComponentActorFactory = new Mock<ComponentActorFactory<Components.ConstructionSite>>(Sys);
+            mockComponentActorFactory.Setup(f => f.GetProps(It.IsAny<Components.ConstructionSite>())).Returns(Props.Create(() => new EntityTestActor()));
+            var constructionSite = new Components.ConstructionSite();
+            var buildOrder = new Systems.Orders.Build(constructionSite, mockComponentActorFactory.Object);
+            buildOrder.Update(entity, new Xenko.Games.GameTime());
+            ExpectMsg<Systems.Construction.Messages.AdvanceProgress>();
+        }
+    }
+}
---
+++ BuildingEconomy/BuildingEconomy.Game/Systems/Orders/Build.cs
@@ -0,0 +1,43 @@
+﻿using Akka.Actor;
+using BuildingEconomy.Systems.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xenko.Engine;
+using Xenko.Games;
+
+namespace BuildingEconomy.Systems.Orders
+{
+    public class Build : Interfaces.IOrder
+    {
+        private readonly Components.ConstructionSite target;
+        private readonly ComponentActorFactory<Components.ConstructionSite> componentActorFactory;
+
+        public Build(Components.ConstructionSite target, ComponentActorFactory<Components.ConstructionSite> componentActorFactory)
+        {
+            this.target = target;
+            this.componentActorFactory = componentActorFactory;
+        }
+
+        public bool IsComplete(Entity entity)
+        {
+            return target.CurrentStageProgress >= 1.0f;
+        }
+
+        public bool IsValid(Entity entity)
+        {
+            // TODO: Add a builder component and check if entity has it.
+            return entity != null;
+        }
+
+        public void Update(Entity entity, GameTime updateTime)
+        {
+            // TODO: Move to entity if not near.
+            IActorRef targetActor = componentActorFactory.GetOrCreateActorForComponent(target);
+
+            targetActor.Tell(new Construction.Messages.AdvanceProgress());
+        }
+    }
+}
